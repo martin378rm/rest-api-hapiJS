c32d4b1fbbc742dcdc7a3d5b07aeebdf34e760cb
martin378rm <martinrivaldo112@gmail.com>
2023-05-04 16:41:02 +0700

    inital commit

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const Hapi = require("@hapi/hapi")
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  })
+
+  server.route({
+    method: "GET",
+    path: "/routes",
+    handler: (request, h) => {
+      const data = {
+        payload: {
+          nama: "Nanang",
+          usia: 40,
+          alamat: "Jonggol",
+        },
+      }
+      return data
+    },
+  })
+
+  await server.start()
+  console.log("Server running on %s", server.info.uri)
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
